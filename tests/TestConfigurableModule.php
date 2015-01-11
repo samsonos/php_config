64b13652641b7b3943f07aa09299080a9afa717c
Vitaly Egorov <egorov@samsonos.com>
2015-01-11 09:33:49 +0200

    Fixed tests

--- tests/TestConfigurableModule.php
+++ tests/TestConfigurableModule.php
@@ -7,20 +7,19 @@
  */
 namespace tests;
 
-use samsonos\config\Entity;
-
-class TestConfigurableModule implements \samsonos\config\IConfigurable
+class TestConfigurableModule
 {
     public $parameterString = '5';
     public $parameterInt = 5;
     public $parameterArray = array('test' => '4');
 
     /**
-     * @param Entity $entityConfiguration current instance for configuration
+     * @param mixed $entityConfiguration current instance for configuration
      * @return boolean False if something went wrong otherwise true
      */
-    public function configure(Entity $entityConfiguration)
+    public function configure($entityConfiguration)
     {
         $this->parameterArray['configurable'] = 'custom configure implementation';
+        $this->parameterArray = array_merge($this->parameterArray, (array)$entityConfiguration);
     }
 }
